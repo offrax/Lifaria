12c60a09f37a69a18ff67b7f4cbc9818baadc5d7
Lifaria <43892282+offrax@users.noreply.github.com>
2022-07-04 18:14:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,87 @@
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menu = document.querySelector(".menu-list");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      menu.classList.add("active");
+      menuBtn.classList.add("hide");
+      cancelBtn.classList.add("show");
+      body.classList.add("disabledScroll");
+    }
+    cancelBtn.onclick = ()=>{
+      menu.classList.remove("active");
+      menuBtn.classList.remove("hide");
+      cancelBtn.classList.remove("show");
+      body.classList.remove("disabledScroll");
+    }
+
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+
+
+    const loader = document.querySelector('.loader');
+
+window.addEventListener('load', () => {
+
+    loader.classList.add('fondu-out');
+
+})
+
+// boutton copier
+var btncopy = document.querySelector('.js-copy');
+if(btncopy) {
+    btncopy.addEventListener('click', docopy);
+}
+
+function docopy() {
+
+    // Cible de l'élément qui doit être copié
+    var target = this.dataset.target;
+    var fromElement = document.querySelector(target);
+    if(!fromElement) return;
+
+    // Sélection des caractères concernés
+    var range = document.createRange();
+    var selection = window.getSelection();
+    range.selectNode(fromElement);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    try {
+        // Exécution de la commande de copie
+        var result = document.execCommand('copy');
+        if (result) {
+            // La copie a réussi
+            alert('L ip du serveur a bien été copié!!');
+        }
+    }
+    catch(err) {
+        // Une erreur est surevnue lors de la tentative de copie
+        alert(err);
+    }
+
+    // Fin de l'opération
+    selection = window.getSelection();
+    if (typeof selection.removeRange === 'function') {
+        selection.removeRange(range);
+    } else if (typeof selection.removeAllRanges === 'function') {
+        selection.removeAllRanges();
+    }
+}
+
+
+// wikinews
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
